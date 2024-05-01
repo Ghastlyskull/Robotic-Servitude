3fc3ef6a34154e5a5b599ec913decd90001dc73e
Taranchuk <nfhfyxer@gmail.com>
2024-05-02 02:28:56 +0300

    make mechs to switch and equip weapons

---
+++ Source/ITab_Pawn_Gear_CanControlColonist.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+namespace RoboticServitude
+{
+    [HarmonyPatch(typeof(ITab_Pawn_Gear), "CanControlColonist", MethodType.Getter)]
+    public static class ITab_Pawn_Gear_CanControlColonist
+    {
+        public static void Postfix(ITab_Pawn_Gear __instance, ref bool __result)
+        {
+            Pawn pawn = __instance.SelPawnForGear;
+            var comp = pawn.GetComp<CompCanEquipWeapons>();
+            if (comp != null)
+            {
+                __result = true;
+            }
+        }
+    }
+
+}
