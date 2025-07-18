888c7f459a50b08086810e53c313bfc10560da45
Nalzurin <alexnormandi@ukr.net>
2025-07-18 21:47:17 +0300

    Implement equipping warcasket boxes

---
+++ Source/VFEPCompat/FloatMenuOptionProvider_FromThingMechBox.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using RoboticServitude;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using VFEPirates;
+
+namespace VFEPCompat
+{
+    public class FloatMenuOptionProvider_FromThingMechBox : FloatMenuOptionProvider_FromThing
+    {
+        public override bool SelectedPawnValid(Pawn pawn, FloatMenuContext context)
+        {
+            return pawn.HasComp<CompCanEquipWeapons>();
+        }
+        protected override bool MechanoidCanDo => true;
+        public override bool TargetThingValid(Thing thing, FloatMenuContext context)
+        {
+            return thing.HasComp<CompWeaponBox>();
+        }
+        public override IEnumerable<FloatMenuOption> GetOptionsFor(Pawn clickedPawn, FloatMenuContext context)
+        {
+            Log.Message("test");
+
+            return base.GetOptionsFor(clickedPawn, context);
+        }
+    }
+}
